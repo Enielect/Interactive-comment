f4ab1d1fff063e689bf244c7b36f3e8e9edf223a
eniola <eniolafaithful57@gmail.com>
2024-04-08 17:54:43 +0100

    implemented contex API with useReducer(although not completely)

--- src/contexts/CommentData.tsx
+++ src/contexts/CommentData.tsx
@@ -1,40 +1,110 @@
-import { createContext, useContext, useReducer } from "react";
+import axios from "axios";
+import { createContext, useEffect, useReducer } from "react";
 
-interface CommentContextProp {
+import { Comment } from "../interfaces/commentInterface";
+
+type MyActionTypes =
+  | { type: actionType.ADD_COMMENT; payload: Comment[] } // Payload for adding a TODO
+  | { type: actionType.ADD_USER_DATA; payload: Comment["user"] } // Payload for removing a TODO
+  | { type: actionType.CHANGE_MODAL_STATE };
+
+//not sure of the below
+
+interface CommentProviderProp {
   // Add your component props here
   children: React.ReactNode;
 }
-const Context = createContext();
 
-const CommentContext: React.FC<CommentContextProp> = ({ children }) => {
-  const initialState = {
-    comment: []
-  }
+interface State {
+  //be careful of the below, what is the consequence of undefined?
+  comments: Comment[] | undefined;
+  userData: Comment["user"];
+  isModalOpen: boolean;
+}
 
-  function reducer(action, state) {
+const initialState = {
+  comments: [],
+  userData: {
+    image: { png: "", webp: "" },
+    username: "",
+  },
+  isModalOpen: false,
+};
+
+const CommentContext = createContext<{
+  data: State;
+  dispatch: React.Dispatch<any>;
+}>({ data: initialState, dispatch: () => null });
+
+enum actionType {
+  ADD_COMMENT = "ADD_COMMENT",
+  ADD_USER_DATA = "ADD_USER_DATA",
+  CHANGE_MODAL_STATE = "CHANGE_MODAL_STATE",
+}
+
+
+
+const CommentProvider: React.FC<CommentProviderProp> = ({ children }) => {
+  function reducer(state: State, action: MyActionTypes): State {
     switch (action.type) {
-      case "ADD_COMMENT":
+      case actionType.ADD_COMMENT:
         return {
           ...state,
-          comments: state.comments.concat(action.payload),
+          comments: action.payload,
         };
+      case actionType.ADD_USER_DATA:
+        return {
+          ...state,
+          userData: action.payload,
+        };
+      case actionType.CHANGE_MODAL_STATE:
+        return { ...state, isModalOpen: !state.isModalOpen };
       default:
-        return new Error("Action not found");
+        return state;
     }
   }
-
+  //fetching comments and userdata
   const [data, dispatch] = useReducer(reducer, initialState);
+
+  // const [comments, setComments] = useState<Comment[] | undefined>(undefined);
+
+  useEffect(() => {
+    // using axios
+    const getComments = async () => {
+      const response = await axios.get("http://localhost:4001/comments");
+      dispatch({ type: actionType.ADD_COMMENT, payload: response.data });
+      // setComments(response.data);
+    };
+    getComments();
+  }, [data.isModalOpen]);
+
+  useEffect(() => {
+    //using axios
+    const getUserData = async () => {
+      const response = await axios.get("http://localhost:4001/currentUser");
+      dispatch({ type: actionType.ADD_USER_DATA, payload: response.data });
+      // setUserData(response.data);
+    };
+    getUserData();
+  }, [data.isModalOpen]);
+
   return (
-    <Context.Provider value={(data, dispatch)}>{children}</Context.Provider>
+    <CommentContext.Provider value={{ data, dispatch }}>
+      {children}
+    </CommentContext.Provider>
   );
 };
 
-function UseCommentsData() {
-  const context = useContext(Context);
-  if (context === undefined) {
-    throw new Error("useCommentsData must be used within a CommentsProvider");
-  }
-  return context;
-}
+//i had issues implementing the below in typescript
+//i ommited this due to the fact that in whatever case we are safe due to 
+//the fact that the Context provider is wrapped arround the whole application
 
-export { CommentContext, UseCommentsData };
+// function UseCommentsData() {
+//   const context = useContext(CommentContext);
+//   if (context === undefined) {
+//     throw new Error("useCommentsData must be used within a CommentsProvider");
+//   }
+//   return context;
+// }
+
+export { CommentProvider, CommentContext };
