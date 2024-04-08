512d16107034a8be8c4ebd077b89b97424bd7229
eniola <eniolafaithful57@gmail.com>
2024-04-08 17:53:31 +0100

    created a file for a bunch of re-usable interfaces

---
+++ src/interfaces/commentInterface.tsx
@@ -0,0 +1,24 @@
+interface userData {
+    image: { png: string; webp: string };
+    username: string;
+  }
+  
+  interface Comment {
+    id: number | string;
+    content: string;
+    createdAt: string;
+    score: number;
+    user: userData;
+    replyingTo?: string;
+    //removing the queston mark from below wasn't the easies of things
+    replies: {
+      id: number;
+      content: string;
+      createdAt: string;
+      score: number;
+      replyingTo: string;
+      user: userData;
+    }[];
+  }
+
+  export type{Comment}
