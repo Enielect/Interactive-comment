4327ef33a5687a8c039e4b2f8c4a9a5fbc425e86
eniola <eniolafaithful57@gmail.com>
2024-04-13 18:41:32 +0100

    completed the update of score on the server as the user alters it in app

--- src/hooks/useLikesUpdate.tsx
+++ src/hooks/useLikesUpdate.tsx
@@ -1,10 +1,50 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { Comment } from "../interfaces/commentInterface";
+import React, { useContext } from "react";
+import { CommentContext } from "../contexts/CommentData";
 
-function useLikesUpdate(likes: number) {
+export default function useLikesUpdate(
+  parentObject: Comment,
+  comment: Comment,
+  likes: number
+) {
+  const { getComments } = useContext(CommentContext);
+  function findUserIndexInReply(): number {
+    return parentObject.replies.findIndex((object) => object.id === comment.id);
+  }
 
-  useEffect(() => {
-    // Add your code here
-    
+  async function updateLikes() {
+    try {
+      let response;
+      if (parentObject) {
+        const index = findUserIndexInReply();
+        response = await axios.put(
+          `http://localhost:4001/comments/${parentObject.id}`,
+          {
+            ...parentObject,
+            replies: [
+              ...parentObject.replies.slice(0, index),
+              { ...parentObject?.replies[index], score: likes },
+              ...parentObject.replies.slice(index + 1),
+            ],
+          }
+        );
+        getComments();
+      } else {
+        response = await axios.put(
+          `http://localhost:4001/comments/${comment.id}`,
+          {
+            ...comment,
+            score: likes,
+          }
+        );
+        getComments();
+      }
+      console.log("likes uploaded successfully:", response);
+    } catch (error) {
+      console.error("Error updating likes:", error);
+    }
+  }
 
-  }, [likes])
+  return { updateLikes };
 }
