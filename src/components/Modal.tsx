f009a83ed2a28e07d045c33d1200dd5977581acb
eniola <eniolafaithful57@gmail.com>
2024-04-15 12:58:32 +0100

    close modal when region ouside the modal card is clicked

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 interface ModalProps {
   // Add your component props here
@@ -8,16 +8,42 @@ interface ModalProps {
   handleDelete: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, handleDelete }) => {
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  children,
+  handleDelete,
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  function handleOutsideModal(e) {
+    e.preventDefault();
+    if (e.target !== ref.current) onClose();
+  }
+
   if (!isOpen) return null;
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-modalBackground flex justify-center items-center z-100">
+    <div
+      onClick={handleOutsideModal}
+      className="fixed top-0 left-0 w-full h-full bg-modalBackground flex justify-center items-center z-100"
+    >
       {/* Your component content here */}
-      <div className="bg-white py-[15px] px-[25px] max-h-[250px] max-w-[75vw] rounded-[10px]">
+      <div
+        ref={ref}
+        className="bg-white py-[15px] px-[25px] max-h-[250px] max-w-[75vw] rounded-[10px]"
+      >
         {children}
         <div className="flex justify-between gap">
-          <button onClick={onClose} className="bg-gray-500 py-[12px] rounded-[9px] px-[15px]">NO, CANCEL</button>
-          <button onClick={handleDelete} className="bg-red-400 py-[12px] px-[15px] rounded-[9px]">
+          <button
+            onClick={onClose}
+            className="bg-gray-500 py-[12px] rounded-[9px] px-[15px]"
+          >
+            NO, CANCEL
+          </button>
+          <button
+            onClick={handleDelete}
+            className="bg-red-400 py-[12px] px-[15px] rounded-[9px]"
+          >
             YES, DELETE
           </button>
         </div>
